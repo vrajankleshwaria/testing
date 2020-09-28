3aa7fb8042b1e305c56a895d1ad6c838342db4b1
Vraj Khatri <vraj2001sa@gmail.com>
2020-09-28 17:21:16 +0530

    Contest_added

--- lib/Screens/PastContests.dart
+++ lib/Screens/PastContests.dart
@@ -1,5 +1,7 @@
-import 'package:codeforces/Common/MyDrawer.dart';
+import 'dart:convert';
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class PastContests extends StatefulWidget {
   @override
@@ -7,13 +9,144 @@ class PastContests extends StatefulWidget {
 }
 
 class _PastContestsState extends State<PastContests> {
+
+  Future<Map<String, dynamic>> contestInfo() async {
+    String url = "https://codeforces.com/api/contest.list";
+    http.Response data = await http.get(url);
+    return json.decode(data.body);
+  }
+
+  String startTime(timestamp) {
+    var _format = new DateFormat('EEE d-MM-y hh:mm a');
+    var date = new DateTime.fromMillisecondsSinceEpoch(timestamp * 1000);
+    var time = _format.format(date);
+    return time;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      drawer: MyDrawer(),
-      body: Center(
-        child: Text('Past Contests'),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        title: Text('Past Contests'),
+      ),
+
+      body: FutureBuilder(
+        future: contestInfo(),
+        builder: (BuildContext context,AsyncSnapshot snapshot){
+          if (snapshot.data == null) {
+            return Center(
+              child: CircularProgressIndicator(
+                backgroundColor: Colors.blueAccent,
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.indigo),
+              ),
+            );
+          }
+          else {
+            return ListView.separated(
+              itemBuilder: (context,index){
+                if(snapshot.data['result'][index]['phase']=="BEFORE"||snapshot.data['result'][index]['phase']=="CODING")
+                {
+                  return Container();
+                }
+                else
+                {
+                  return Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 2,
+                            spreadRadius: 3,
+                            color: Colors.green.withOpacity(0.2),
+                          ),
+                        ],
+                      ),
+                      margin: EdgeInsets.fromLTRB(20,12,20,12),
+                      padding: EdgeInsets.fromLTRB(8,8,8,8),
+                      child: Row(
+                        children: <Widget>[
+                          Column(
+                            children: <Widget>[
+                              Container(
+                                child:Text(
+                                  'Contest Name',
+                                  style: TextStyle(fontFamily: 'Robooto',fontSize: 15),
+                                ),
+                              ),
+                              Container(
+                                child:Text(
+                                  'Platform',
+                                  style: TextStyle(fontFamily: 'Robooto',fontSize: 15),
+                                ),
+                              ),
+                              Container(
+                                child:Text(
+                                  'Duration',
+                                  style: TextStyle(fontFamily: 'Robooto',fontSize: 15),
+                                ),
+                              ),
+                              Container(
+                                child:Text(
+                                  'StartTime',
+                                  style: TextStyle(fontFamily: 'Robooto',fontSize: 15),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(width: 15,),
+                          Column(
+                            children: <Widget>[
+                              Container(child: Text(':',style: TextStyle(fontSize: 16),),),
+                              Container(child: Text(':',style: TextStyle(fontSize: 16),),),
+                              Container(child: Text(':',style: TextStyle(fontSize: 16),),),
+                              Container(child: Text(':',style: TextStyle(fontSize: 16),),),
+                            ],
+                          ),
+                          SizedBox(width: 15,),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Container(
+                                  child:Text(
+                                    snapshot.data['result'][index]['name'],
+                                    style: TextStyle(fontWeight: FontWeight.bold,color: Colors.blue),
+                                  ),
+                                ),
+                                Container(
+                                  child:Text(
+                                    snapshot.data['result'][index]['type'],
+                                    style: TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Container(
+                                  child:Text(
+                                    (snapshot.data['result'][index]['durationSeconds']/3600).toString()+' Hr',
+                                    style: TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Container(
+                                  child:Text(
+                                    startTime(snapshot.data['result'][index]['startTimeSeconds']),
+                                    style: TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      )
+                  );
+                }
+                return null;
+              },
+              separatorBuilder: (_, __) => Divider(height: 0.5),
+              itemCount: snapshot.data['result']==null ? 0 : snapshot.data['result'].length,
+            );
+          }
+        },
       ),
     );
   }
--- lib/Screens/ShowContest.dart
+++ lib/Screens/ShowContest.dart
@@ -9,7 +9,6 @@ class ShowContest extends StatefulWidget {
 }
 
 class _ShowContestState extends State<ShowContest> {
-
   Future<Map<String, dynamic>> contestInfo() async {
     String url = "https://codeforces.com/api/contest.list";
     http.Response data = await http.get(url);
@@ -33,105 +32,115 @@ class _ShowContestState extends State<ShowContest> {
 
       body: FutureBuilder(
         future: contestInfo(),
-        builder: (context,snapshot){
-          return ListView.separated(
+        builder: (BuildContext context,AsyncSnapshot snapshot){
+          if (snapshot.data == null) {
+            return Center(
+              child: CircularProgressIndicator(
+                backgroundColor: Colors.blueAccent,
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.indigo),
+              ),
+            );
+          }
+          else {
+            return ListView.separated(
               itemBuilder: (context,index){
                 if(snapshot.data['result'][index]['phase']=='BEFORE')
                 {
                   return Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(15),
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 2,
-                          spreadRadius: 3,
-                          color: Colors.green.withOpacity(0.2),
-                        ),
-                      ],
-                    ),
-                    margin: EdgeInsets.fromLTRB(20,12,20,12),
-                    padding: EdgeInsets.fromLTRB(8,8,8,8),
-                    child: Row(
-                      children: <Widget>[
-                        Column(
-                          children: <Widget>[
-                            Container(
-                              child:Text(
-                                'Contest Name',
-                                style: TextStyle(fontFamily: 'Robooto',fontSize: 15),
-                              ),
-                            ),
-                            Container(
-                              child:Text(
-                                'Platform',
-                                style: TextStyle(fontFamily: 'Robooto',fontSize: 15),
-                              ),
-                            ),
-                            Container(
-                              child:Text(
-                                'Duration',
-                                style: TextStyle(fontFamily: 'Robooto',fontSize: 15),
-                              ),
-                            ),
-                            Container(
-                              child:Text(
-                                'StartTime',
-                                style: TextStyle(fontFamily: 'Robooto',fontSize: 15),
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(width: 15,),
-                        Column(
-                          children: <Widget>[
-                            Container(child: Text(':',style: TextStyle(fontSize: 16),),),
-                            Container(child: Text(':',style: TextStyle(fontSize: 16),),),
-                            Container(child: Text(':',style: TextStyle(fontSize: 16),),),
-                            Container(child: Text(':',style: TextStyle(fontSize: 16),),),
-                          ],
-                        ),
-                        SizedBox(width: 15,),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 2,
+                            spreadRadius: 3,
+                            color: Colors.green.withOpacity(0.2),
+                          ),
+                        ],
+                      ),
+                      margin: EdgeInsets.fromLTRB(20,12,20,12),
+                      padding: EdgeInsets.fromLTRB(8,8,8,8),
+                      child: Row(
+                        children: <Widget>[
+                          Column(
                             children: <Widget>[
                               Container(
                                 child:Text(
-                                  snapshot.data['result'][index]['name'],
-                                  style: TextStyle(fontWeight: FontWeight.bold,color: Colors.blue),
+                                  'Contest Name',
+                                  style: TextStyle(fontFamily: 'Robooto',fontSize: 15),
                                 ),
                               ),
                               Container(
                                 child:Text(
-                                  snapshot.data['result'][index]['type'],
-                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                  'Platform',
+                                  style: TextStyle(fontFamily: 'Robooto',fontSize: 15),
                                 ),
                               ),
                               Container(
                                 child:Text(
-                                  (snapshot.data['result'][index]['durationSeconds']/3600).toString()+' Hr',
-                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                  'Duration',
+                                  style: TextStyle(fontFamily: 'Robooto',fontSize: 15),
                                 ),
                               ),
                               Container(
                                 child:Text(
-                                  startTime(snapshot.data['result'][index]['startTimeSeconds']),
-                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                  'StartTime',
+                                  style: TextStyle(fontFamily: 'Robooto',fontSize: 15),
                                 ),
                               ),
                             ],
                           ),
-                        ),
-                      ],
-                    )
+                          SizedBox(width: 15,),
+                          Column(
+                            children: <Widget>[
+                              Container(child: Text(':',style: TextStyle(fontSize: 16),),),
+                              Container(child: Text(':',style: TextStyle(fontSize: 16),),),
+                              Container(child: Text(':',style: TextStyle(fontSize: 16),),),
+                              Container(child: Text(':',style: TextStyle(fontSize: 16),),),
+                            ],
+                          ),
+                          SizedBox(width: 15,),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Container(
+                                  child:Text(
+                                    snapshot.data['result'][index]['name'],
+                                    style: TextStyle(fontWeight: FontWeight.bold,color: Colors.blue),
+                                  ),
+                                ),
+                                Container(
+                                  child:Text(
+                                    snapshot.data['result'][index]['type'],
+                                    style: TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Container(
+                                  child:Text(
+                                    (snapshot.data['result'][index]['durationSeconds']/3600).toString()+' Hr',
+                                    style: TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Container(
+                                  child:Text(
+                                    startTime(snapshot.data['result'][index]['startTimeSeconds']),
+                                    style: TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      )
                   );
                 }
                 return null;
               },
               separatorBuilder: (_, __) => Divider(height: 0.5),
               itemCount: snapshot.data['result']==null ? 0 : snapshot.data['result'].length,
-          );
+            );
+          }
         },
       ),
     );
